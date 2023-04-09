8713603071fdfd4366ecac72075d37ec6af175ee
anggriawanmr <anggriawanmr@gmail.com>
2023-04-09 14:59:21 +0700

    Editing CustomButton component :hammer:

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
   const generateStyle = (type) => {
     if (type === 'filled') {
       return {
-        backgroundColor: '#000',
+        backgroundColor: snap.color,
         color: '#fff',
       };
     }
@@ -14,6 +18,7 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
       style={generateStyle(type)}
+      onClick={handleClick}
     >
       {title}
     </button>
