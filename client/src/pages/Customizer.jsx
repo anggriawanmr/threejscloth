77c28e90a7639f3a919f3b8c87079e7499c3d835
anggriawanmr <anggriawanmr@gmail.com>
2023-04-12 13:27:33 +0700

    Editing ColorPicker component :hammer:

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -31,7 +31,18 @@ const Customizer = () => {
   });
 
   // Show tab content depending on the activeTab
-  const generateTabContent = () => {};
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />;
+      case 'filepicker':
+        return <FilePicker />;
+      case 'aipicker':
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <AnimatePresence>
@@ -45,8 +56,14 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
